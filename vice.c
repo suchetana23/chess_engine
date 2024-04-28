7a743acb876c474bf4cd77d16bfeec1affd78beb
Suchetana Chowdhury <shovanchowdhury@suchetana-MacBook-Air.local>
2024-04-28 13:05:33 +0530

    change in main

--- vice.c
+++ vice.c
@@ -3,20 +3,6 @@
 
 int main(){
     
-    AllInit();  
-    int index; 
-    for (index=0;index<BRD_SQ_NUM;++index){
-        if (index%10==0) printf("\n");
-        printf("%5d", Sq120ToSq64[index]);
-    }
-
-    printf("\n");
-    printf("\n");
-
-    for (index=0;index<64;++index){
-        if(index%8==0)printf("\n");
-        printf("%5d", Sq64ToSq120[index]);
-    }
-    
+    AllInit();
     return 0;
 }
