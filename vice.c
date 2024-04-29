e4b67f313d15821cf3126973242b005230a9ae21
Suchetana Chowdhury <shovanchowdhury@suchetana-MacBook-Air.local>
2024-04-30 00:04:13 +0530

    clear and set bit

--- vice.c
+++ vice.c
@@ -5,17 +5,19 @@ int main(){
     
     AllInit();
     U64 playBitBoard = 0ULL;
-
-    playBitBoard |= (1ULL <<SQ64(D2));
-    playBitBoard |= (1ULL <<SQ64(D3));
-    playBitBoard |= (1ULL <<SQ64(D4));
-    
-    int sq64=0;
-    while (playBitBoard){
-        sq64 = POP(&playBitBoard);
-        printf("popped:%d\n", sq64);
-        PrintBitBoard(playBitBoard);
+    int index=0;
+/*
+    for (index=0;index<64;index++){
+        printf("Index:%d\n", index);
+        PrintBitBoard(ClearMask[index]);
+        printf("\n");
     }
+*/
+    
+    SETBIT(playBitBoard,61);
+    PrintBitBoard(playBitBoard);
+    CLRBIT(playBitBoard,61);
+    PrintBitBoard(playBitBoard);
 
     return 0;
 }
