13ed5cf6fde899e2bcb6cbad2624f0a790f8ee49
Suchetana Chowdhury <shovanchowdhury@suchetana-MacBook-Air.local>
2024-05-16 16:22:10 +0530

    change in RAND_64

--- init.c
+++ init.c
@@ -1,10 +1,10 @@
 #include "defs.h"
 #include <stdlib.h>
 
-#define RAND_64 (   (U64)rand() + \
-                    (U64)rand() << 15 + \
-                    (U64)rand() << 30 + \
-                    (U64)rand() << 45 + \
+#define RAND_64 (   (U64)rand() | \
+                    (U64)rand() << 15 | \
+                    (U64)rand() << 30 | \
+                    (U64)rand() << 45 | \
                     ((U64)rand() & 0xf) << 60   )
 
 int Sq120ToSq64[BRD_SQ_NUM];
