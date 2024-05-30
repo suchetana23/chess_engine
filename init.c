bb1a30b12d612a129c8d0ef6cc38fc47f22abf6a
Suchetana Chowdhury <shovanchowdhury@suchetana-MacBook-Air.local>
2024-05-30 19:42:03 +0530

    Rank and file array

--- init.c
+++ init.c
@@ -1,5 +1,6 @@
 #include "defs.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define RAND_64 (   (U64)rand() | \
                     (U64)rand() << 15 | \
@@ -16,6 +17,42 @@ U64 ClearMask[64];
 U64 PieceKeys[130][120];
 U64 SideKey;
 U64 CastleKeys[16];
+
+int FilesBrd[BRD_SQ_NUM];
+int RanksBrd[BRD_SQ_NUM];
+
+void InitFilesRanksBrd() {
+    int index = 0;
+    int file = FILE_A;
+    int rank = RANK_1;
+    int sq = A1;
+    int sq64 = 0;
+
+    for (int i=0;i<BRD_SQ_NUM;++i){
+        FilesBrd[i] = OFFBOARD;
+        RanksBrd[i] = OFFBOARD;
+    }
+
+    for (int rank = RANK_1;rank<=RANK_8;++rank){
+        for(int file = FILE_A;file <= FILE_H;++file){
+            sq = FR2SQ(file,rank);
+            FilesBrd[sq] = file;
+            RanksBrd[sq] = rank;
+        }
+    }
+
+    printf("FilesBrd\n");
+    for(int i=0; i<BRD_SQ_NUM;++i){
+        if(i%10==0 && i!=0) printf("\n");
+        printf("%4d", FilesBrd[i]);
+    }
+
+    printf("RankBrd\n");
+    for(int i=0; i<BRD_SQ_NUM; ++i){
+        if(i%10==0 && i!=0) printf("\n");
+        printf("%4d", RanksBrd[i]); 
+    }
+}
 
 void InitHashKeys(){
 
@@ -75,4 +112,5 @@ void AllInit(){
     InitSq120To64();
     InitBitMasks();
     InitHashKeys();
+    InitFilesRanksBrd();
 }
